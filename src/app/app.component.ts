118ff62c98efa456086ce6c16383105f5cf91aba
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2021-01-09 12:07:34 +0100

    Add @HostListener for navbar color change on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, HostListener } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { TmdbService } from './services/tmdb.service';
 import { faPlay, faSearch, faGift, faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
@@ -24,6 +24,20 @@ export class AppComponent implements OnInit {
 
 	constructor(private apiService: TmdbService){
 
+	}
+
+	@HostListener('window:scroll', ['$event']) 
+	onScroll(e: Event): void {
+		let navbar = (document.getElementById('navbar'));
+		let position = (document.documentElement.scrollTop || document.body.scrollTop);
+		console.log(navbar, position);
+		if(position >= 200){
+			navbar.classList.remove('bg-transparent');
+			navbar.classList.add('bg-black');
+		}else{
+			navbar.classList.add('bg-transparent');
+			navbar.classList.remove('bg-black');
+		}
 	}
 
 	ngOnInit(): void{
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -6,8 +6,8 @@ import { environment } from './environments/environment';
 import {CommonModule} from '@angular/common';
 
 if (environment.production) {
-  enableProdMode();
+	enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+	.catch(err => console.error(err));
