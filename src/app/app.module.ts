2125f7f2dc5157f2333745272fc13f951d16c0b1
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2021-01-26 11:09:32 +0100

    Clean & delete angular browser animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardComponent } from './card/card.component';
 
 @NgModule({
@@ -18,7 +17,6 @@ import { CardComponent } from './card/card.component';
 		BrowserModule,
 		AppRoutingModule,
 		HttpClientModule,
-		BrowserAnimationsModule,
 		FontAwesomeModule
 	],
 	exports: [
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,52 +3,47 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 import { faTimesCircle, faPlus, faThumbsUp, faThumbsDown, faPlay } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.sass']
+	selector: 'app-card',
+	templateUrl: './card.component.html',
+	styleUrls: ['./card.component.sass']
 })
 export class CardComponent implements OnInit {
-  @Input() movie: any;
-  @Input() genres: any;
-  
-  //icons
-  faTimesCircle = faTimesCircle;
-  faPlus = faPlus;
-  faThumbsUp = faThumbsUp;
-  faThumbsDown = faThumbsDown;
-  faPlay = faPlay;
+	@Input() movie: any;
+	@Input() genres: any;
+
+	//icons
+	faTimesCircle = faTimesCircle;
+	faPlus = faPlus;
+	faThumbsUp = faThumbsUp;
+	faThumbsDown = faThumbsDown;
+	faPlay = faPlay;
 
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-    this.displayGenres();
-  }
+	ngOnInit(): void {
+	this.displayGenres();
+	}
 
-  //genres
-  displayGenres(): string{
-    let itemGenres = this.movie.genre_ids;
-    let genresFromAPI = this.genres.genres;
-    let result: string = '';
+	//genres
+	displayGenres(): string{
+	let itemGenres = this.movie.genre_ids;
+	let genresFromAPI = this.genres.genres;
+	let result: string = '';
 
-    //genresArray.forEach(element => console.log(element));
+	//genresArray.forEach(element => console.log(element));
 
-    for(let i = 0; i < itemGenres.length; i++){
-
+	for(let i = 0; i < itemGenres.length; i++){
 		let genreId = itemGenres[i];
-
 		for(let y = 0; y < genresFromAPI.length; y++){
-
 			let actualGenre = genresFromAPI[y].id
-
 			if(genreId == actualGenre){
-
 				result += genresFromAPI[y].name + ', ';
 			}
 		}
-    }
+	}
 
-    return result;
-  }
+	return result;
+	}
 
 }
